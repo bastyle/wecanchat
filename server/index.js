0d67df04e1d24429f289a1361e204235048311e0
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-02-04 12:23:03 -0500

    added mongodb atlas connection, added test user to the collection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const cors = require("cors")
 require('dotenv').config()
+const { default: mongoose } = require('mongoose')
+const UserModel = require('./model/userModel')
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+// DB Connection
+console.log('Connecting to MongoDB Atlas...');
+mongoose.connect(process.env.MONGODB_URL,{
+  useNewUrlParser : true ,
+  useUnifiedTopology : true
+}).then(()=>{
+  console.log('connected to mongoDB atlas instance.');
+  // adding test user
+  /*const userTest = new UserModel({username:'Bastian', email:'admin@admin.com',password:'admin2024'})
+  userTest.save().then(doc => {
+      //console.log('user test (admin) saved:', doc);
+      console.log('user test (admin) saved:');
+    })
+    .catch(error => {
+      console.error('Error saving user:', error);
+    });*/
+});
 
 //test endpoint 
 app.get("/api/health", (req, res) => {
