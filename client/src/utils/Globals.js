11448d09eb50352c414f7ca0d3cbad25af775a03
Bastian Bastias <bastian.bastias@gmail.com>
2024-03-10 15:43:41 -0400

    Feature/integratation (#11)
    * added logs
    
    * added logs
    
    * updated postman collection
    
    * homepage
    
    * fixed login signup components
    
    * fixed announcements links
    
    * fixed login user json return
    
    * added utils API routes
    
    * fixed login flow
    
    * changed http return code
    
    * fixed signup flow
    
    * added logout flow
    
    * added navbar logic
    
    * commented guest link
    
    * fixed chat feature
    
    * fixed chat feature
    
    * integrated code. (#8) (#9)
    
    * added logs
    
    * added logs
    
    * updated postman collection
    
    * homepage
    
    * fixed login signup components
    
    * fixed announcements links
    
    * fixed login user json return
    
    * added utils API routes
    
    * fixed login flow
    
    * changed http return code
    
    * fixed signup flow
    
    * added logout flow
    
    * added navbar logic
    
    * commented guest link
    
    * fixed chat feature
    
    * added toast messages
    
    * fixed profile feature
    
    * fixes css

---
+++ client/src/utils/Globals.js
@@ -0,0 +1,10 @@
+export const genericToastOptions = {
+    position: "top-center",
+    autoClose: 1500,
+    pauseOnHover: true,
+    draggable: false,
+    theme: "light",
+    onClose: () => {
+      console.log("options");
+    }
+  };
