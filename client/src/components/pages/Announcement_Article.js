eb2e0cdf1b21c9c98e2e2b6d7f14e88241c2ef17
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-03-14 12:38:48 -0400

    added announcement content

--- client/src/components/pages/Announcement_Article.js
+++ client/src/components/pages/Announcement_Article.js
@@ -7,16 +7,21 @@ import { announcementRoute } from "../../utils/APIRoutes";
 function Announcement_Article() {
   const { id } = useParams(); // Get the article ID from the URL params
   const [article, setArticle] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   console.log("Announcement_Article.js: id:", id);
 
   useEffect(() => {
+    console.log("Fetching article details for ID:", id);
     // Fetch the specific article details using the article ID
     fetch(`${announcementRoute}/${id}`)
       .then((response) => response.json())
       .then((data) => {
         // Handle the response data
-        console.log("Article details:", data);
+        //console.log("Article details:", data);
+        console.log("Article title:", data.title);
         setArticle(data);
+        setIsLoaded(true);
+        //console.log("Article title:", article.title);
       })
       .catch((error) => {
         // Handle any errors
@@ -27,10 +32,35 @@ function Announcement_Article() {
   return (
     <div>
       <Navbar />
-      <h1>Article ID: {id}</h1>
-      <h3>{article.title}</h3>
-      {/* Display the details of the specific article */}
+      <div className="Article">
+        {isLoaded ? (
+          <div>
+            <div className="Article-Container">
+              <div className="Article-Header-Box">
+                <h1 className="Article-Title">{article.title}</h1>
+                <h3 className="Article-Subtitle">
+                  {article.author.charAt(0).toUpperCase() + article.author.slice(1)} |{" "}
+                  {new Date(article.createdDate).toLocaleDateString()}
+                </h3>
+              </div>
+              <div className="image-container">
+                <img
+                  className="thumbnail2"
+                  src={`data:image/svg+xml;base64,${article.image}`}
+                  alt="thumbnail"
+                />
+              </div>
+              <div className="content-container">
+                <p className="article-content">{article.content}</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </div>
   );
+
 }
 export default Announcement_Article;
