892dfdda692566f86e41a6bb5ccf644215dfec14
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-04-06 22:04:21 -0400

    fixed announcement call function isAdminUser

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -118,22 +118,23 @@ function Navbar() {
         );
     } else {
         return (
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/announcements">Announcements</Link>
-                    </li>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="main-container">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/announcements">Announcements</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         );
     }
-
 
 }
 
